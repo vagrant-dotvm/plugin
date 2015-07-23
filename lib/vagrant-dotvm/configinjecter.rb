b5ad5a5329c96e062255382c020036d3a2a10bbd
Krzysztof Magosa <krzysztof@magosa.pl>
2015-07-23 21:10:14 +0200

    refactor for easier extension

--- lib/vagrant-dotvm/configinjecter.rb
+++ lib/vagrant-dotvm/configinjecter.rb
@@ -22,24 +22,15 @@ module VagrantPlugins
           define_opts[:autostart] = machine_cfg.autostart unless machine_cfg.autostart.nil?
 
           vc.vm.define machine_cfg.nick, **define_opts do |machine|
-            machine.vm.box                           = machine_cfg.box                           unless machine_cfg.box.nil?
-            machine.vm.hostname                      = machine_cfg.hostname                      unless machine_cfg.hostname.nil?
-            machine.vm.boot_timeout                  = machine_cfg.boot_timeout                  unless machine_cfg.boot_timeout.nil?
-            machine.vm.box_check_update              = machine_cfg.box_check_update              unless machine_cfg.box_check_update.nil?
-            machine.vm.box_version                   = machine_cfg.box_version                   unless machine_cfg.box_version.nil?
-            machine.vm.graceful_halt_timeout         = machine_cfg.graceful_halt_timeout         unless machine_cfg.graceful_halt_timeout.nil?
-            machine.vm.post_up_message               = machine_cfg.post_up_message               unless machine_cfg.post_up_message.nil?
-            machine.vm.box_download_checksum         = machine_cfg.box_download_checksum         unless machine_cfg.box_download_checksum.nil?
-            machine.vm.box_download_checksum_type    = machine_cfg.box_download_checksum_type    unless machine_cfg.box_download_checksum_type.nil?
-            machine.vm.box_download_client_cert      = machine_cfg.box_download_client_cert      unless machine_cfg.box_download_client_cert.nil?
-            machine.vm.box_download_ca_cert          = machine_cfg.box_download_ca_cert          unless machine_cfg.box_download_ca_cert.nil?
-            machine.vm.box_download_ca_path          = machine_cfg.box_download_ca_path          unless machine_cfg.box_download_ca_path.nil?
-            machine.vm.box_download_insecure         = machine_cfg.box_download_insecure         unless machine_cfg.box_download_insecure.nil?
-            machine.vm.box_download_location_trusted = machine_cfg.box_download_location_trusted unless machine_cfg.box_download_location_trusted.nil?
-            machine.vm.box_url                       = machine_cfg.box_url                       unless machine_cfg.box_url.nil?
-            machine.vm.communicator                  = machine_cfg.communicator                  unless machine_cfg.communicator.nil?
-            machine.vm.guest                         = machine_cfg.guest                         unless machine_cfg.guest.nil?
-            machine.vm.usable_port_range             = machine_cfg.usable_port_range             unless machine_cfg.usable_port_range.nil?
+            [
+              :box, :hostname, :boot_timeout, :box_check_update, :box_version, :graceful_halt_timeout, :post_up_message,
+              :box_download_checksum, :box_download_checksum_type, :box_download_client_cert, :box_download_ca_cert,
+              :box_download_ca_path, :box_download_insecure, :box_download_location_trusted, :box_url, :communicator,
+              :guest, :usable_port_range
+            ].each do |opt|
+              val = machine_cfg.send(opt)
+              machine.vm.send("#{opt}=", val) unless val.nil?
+            end
 
             machine.vm.provider "virtualbox" do |vb|
               vb.customize ["modifyvm", :id, "--memory",          machine_cfg.memory] unless machine_cfg.memory.nil?
@@ -59,18 +50,14 @@ module VagrantPlugins
 
             machine_cfg.networks.each do |net|
               hash = {}
-              hash[:type]               = net.type         unless net.type.nil?
-              hash[:ip]                 = net.ip           unless net.ip.nil?
-              hash[:netmask]            = net.mask         unless net.mask.nil?
-              hash[:virtualbox__intnet] = net.interface    unless net.interface.nil?
-              hash[:guest]              = net.guest        unless net.guest.nil?
-              hash[:host]               = net.host         unless net.host.nil?
-              hash[:protocol]           = net.protocol     unless net.protocol.nil?
-              hash[:bridge]             = net.bridge       unless net.bridge.nil?
-              hash[:guest_ip]           = net.guest_ip     unless net.guest_ip.nil?
-              hash[:host_ip]            = net.host_ip      unless net.host_ip.nil?
-              hash[:auto_correct]       = net.auto_correct unless net.auto_correct.nil?
-              hash[:auto_config]        = net.auto_config  unless net.auto_config.nil?
+
+              [
+                :type, :ip, :netmask, :virtualbox__intnet, :guest, :host, :protocol, :bridge,
+                :guest_ip, :host_ip, :auto_correct, :auto_config,
+              ].each do |opt|
+                val = net.send(opt)
+                hash[opt] = val unless val.nil?
+              end
 
               machine.vm.network net.net, **hash
             end
@@ -112,24 +99,15 @@ module VagrantPlugins
 
             machine_cfg.shared_folders.each do |folder|
               hash = {}
-              hash[:disabled]          = folder.disabled          unless folder.disabled.nil?
-              hash[:create]            = folder.create            unless folder.create.nil?
-              hash[:type]              = folder.type              unless folder.type.nil?
-              hash[:group]             = folder.group             unless folder.group.nil?
-              hash[:mount_options]     = folder.mount_options     unless folder.mount_options.nil?
-              hash[:owner]             = folder.owner             unless folder.owner.nil?
-              hash[:nfs_export]        = folder.nfs_export        unless folder.nfs_export.nil?
-              hash[:nfs_udp]           = folder.nfs_udp           unless folder.nfs_udp.nil?
-              hash[:nfs_version]       = folder.nfs_version       unless folder.nfs_version.nil?
-              hash[:rsync__args]       = folder.rsync__args       unless folder.rsync__args.nil?
-              hash[:rsync__auto]       = folder.rsync__auto       unless folder.rsync__auto.nil?
-              hash[:rsync__chown]      = folder.rsync__chown      unless folder.rsync__chown.nil?
-              hash[:rsync__exclude]    = folder.rsync__exclude    unless folder.rsync__exclude.nil?
-              hash[:rsync__rsync_path] = folder.rsync__rsync_path unless folder.rsync__rsync_path.nil?
-              hash[:rsync__verbose]    = folder.rsync__verbose    unless folder.rsync__verbose.nil?
-              hash[:smb_host]          = folder.smb_host          unless folder.smb_host.nil?
-              hash[:smb_password]      = folder.smb_password      unless folder.smb_password.nil?
-              hash[:smb_username]      = folder.smb_username      unless folder.smb_username.nil?
+
+              [
+                :disabled, :create, :type, :group, :mount_options, :owner, :nfs_export, :nfs_udp, :nfs_version,
+                :rsync__args, :rsync__auto, :rsync__chown, :rsync__exclude, :rsync__rsync_path, :rsync__verbose,
+                :smb_host, :smb_password, :smb_username
+              ].each do |opt|
+                val = folder.send(opt)
+                hash[opt] = val unless val.nil?
+              end
 
               machine.vm.synced_folder folder.host, folder.guest, **hash
             end
