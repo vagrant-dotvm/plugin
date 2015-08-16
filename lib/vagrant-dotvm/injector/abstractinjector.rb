5e809c4543b5eb7561d456764883cb25ebbe0bf8
Krzysztof Magosa <krzysztof@magosa.pl>
2015-08-16 16:26:00 +0300

    add docs to abstractinjector

--- lib/vagrant-dotvm/injector/abstractinjector.rb
+++ lib/vagrant-dotvm/injector/abstractinjector.rb
@@ -2,6 +2,11 @@ module VagrantPlugins
   module Dotvm
     module Injector
       class AbstractInjector
+        # Extracts specified `options` from `source` and return them as hash.
+        #
+        # @param source [Object] Object to extract data from
+        # @param options [Array] List of options to be extraced
+        # @return [Hash] Extracted data
         def self.generate_hash(source, options)
           hash = {}
 
@@ -13,6 +18,11 @@ module VagrantPlugins
           return hash
         end
 
+        # Rewrite `options` from `source` to `target`.
+        #
+        # @param source [Object] Object to rewrite data from
+        # @param target [Object] Object to rewrite data to
+        # @param options [Array] List of options to be rewritten
         def self.rewrite_options(source, target, options)
           options.each do |opt|
             val = source.send(opt)
