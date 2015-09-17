839e7b40b3c91f9523ac906126a9532a7bb15f72
Krzysztof Magosa <krzysztof@magosa.pl>
2015-09-17 18:53:40 +0200

    remove not needed new lines

--- lib/vagrant-dotvm/plugin.rb
+++ lib/vagrant-dotvm/plugin.rb
@@ -1,10 +1,8 @@
 module VagrantPlugins
   module Dotvm
-
     class Plugin < Vagrant.plugin(2)
       name 'Dotvm'
       description 'Easy YAML based multi machine config.'
     end # Plugin
-
   end # Dotvm
 end # VagrantPlugins
